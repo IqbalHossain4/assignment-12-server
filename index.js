6447e79561ba870ac834a5978821044e15b35786
iqbalhossain4 <iqbal-hossain@mail.com>
2023-06-14 16:54:09 +0600

    update

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+//verify jwt
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
