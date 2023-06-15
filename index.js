43794f3ef44d64b1a6a126c17156ede66c1f9080
Iqbal Hossain <121719564+IqbalHossain4@users.noreply.github.com>
2023-06-15 17:46:25 +0600

    add some

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const verifyJWT = (req, res, next) => {
   });
 };
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.axpgb1h.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://lastassignment:KlOEYPzFwMsXvoBu@cluster0.axpgb1h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
